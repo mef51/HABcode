df408023a1389eccf80db61f9de7da3b172aaa24
alexispascual <ap.alexispascual@gmail.com>
2018-04-18 15:38:37 -0400

    Adding Code for Geiger Counter. ADP

---
+++ Geiger Counter/GeigerCounter.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+from datetime import datetime
+from pytz import timezone
+import serial, io
+
+tz = timezone('Canada/Eastern')
+
+addr = '/dev/ttyUSB0'
+baud = 9600
+fname = 'GeigerCounter.txt'
+fmode = 'a'
+
+with serial.Serial(addr,9600) as pt, open(fname,fmode) as outf:
+	spb = io.TextIOWrapper(io.BufferedRWPair(pt,pt,1),
+		encoding = 'ascii', errors = 'ignore', newline = '\r', line_buffering = False)
+	spb.readline()
+	while (1):
+		outf.write('{0}, {1} \n'.format(datetime.now(tz), spb.readline().strip()))
+		outf.flush()
