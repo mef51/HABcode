4f75c5362724b88ac0f427cbf25626519ed8f485
alexispascual <ap.alexispascual@gmail.com>
2018-04-26 16:39:47 -0400

    Adding code for CO2 sensor.

---
+++ CO2/CO2.py
@@ -0,0 +1,22 @@
+import serial
+import time
+ser = serial.Serial("/dev/serial0", baudrate = 9600, timeout = 1)
+ser.flushInput()
+time.sleep(1)
+
+from datetime import datetime
+
+while (1):
+	ser.flushInput()
+	ser.write("\xFE\x44\x00\x08\x02\x9F\x25")
+	time.sleep(.5)
+	resp = ser.read(7)
+	print "@", resp
+	if resp! = ""
+		high = ord(resp[3])
+		low = ord(resp[4])
+		co2 = (high * 256) + low
+		msg = "%s, %s \n" %(datetime.now(), co2)
+		with open ("/home/pi/Desktop/CO2log.txt", "a") as log:
+			log.write(msg)
+	time.sleep(1)
