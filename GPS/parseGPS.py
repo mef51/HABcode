978e757b094b0720fd411cb218331056eba0ba46
Your Name <you@example.com>
2018-05-09 22:08:43 -0400

    Adding APRS packet script as well as sending the packet to the GPS code. ADP

--- GPS/parseGPS.py
+++ GPS/parseGPS.py
@@ -6,8 +6,12 @@
 ###
 import serial
 import pynmea2
+
+from subprocess import call
+from time import sleep
 port = '/dev/ttyS0'
 baud = 9600 # pulse rate of port
+callSign = 'VE3UWO-1'
 
 def handleGPSmsg(GGAmsg, RMCmsg):
 	"""
@@ -27,6 +31,21 @@ def handleGPSmsg(GGAmsg, RMCmsg):
 	latitude = '%02d°%02d′%07.4f″' % (msg.latitude, msg.latitude_minutes, msg.latitude_seconds)
 	
 	print(str(timestmp)+" UTC:", latitude, longitude, 'alt:', altitude, 'meters spd:', '{:.3f}'.format(speed), "m/s")
+
+	message = "Latitude: " + str(latitude) + " Longitude: " + str(longitude) + " Altitude" + str(altitude) + " Speed: " + '{:.3f}'.format(speed) + "m/s"
+
+	if not call(['aprs', '-c', callSign, '-o', 'packet.wav',  message]):
+		print("APRS packet created with message: " + message)
+	else
+		print("There was an error creating the APRS packet")
+
+	if not call(['./sendAprs.sh'])
+		print("APRS packet sent!")
+	else
+		print("APRS packet not sent!")
+
+	sleep(30)
+
 	
 
 if __name__ == '__main__':
---
+++ GPS/sendAPRS.sh
@@ -0,0 +1,29 @@
+#!usr/bin/bash
+
+echo Enabling audio
+sudo modprobe snd-bcm2835
+
+echo Redirecting audio to GPIO pins
+gpio mode 1 alt5
+
+echo Setting audio volume
+amixer set PCM -- 400
+
+echo Preparing transmitter PTT
+gpio mode 4 out
+gpio write 4 0
+
+if [ -e packet.wav ]
+then
+	echo Activating transmitter PTT
+	gpio write 4 1
+
+	echo Playing audio
+	aplay packet.wav
+
+	echo Done playing audio. Removing packet and turning of transmitter
+	rm packet.wav
+	gpio write 4 0
+
+	
+
