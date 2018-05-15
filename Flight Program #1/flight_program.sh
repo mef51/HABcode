eabcb0323ed8a2e3194c14cc80cf1c7512a6a194
Alexis Pascual <ap.alexispascual@gmail.com>
2018-05-15 21:29:02 +0000

    Forgot to run things in background. ADP

--- Flight Program #1/flight_program.sh
+++ Flight Program #1/flight_program.sh
@@ -4,10 +4,10 @@ echo "Starting video."
 raspivid -t 21600000 -vs -w 1640 -h 1232 -fps 30 -b 15000000 -o $(date +"%T")_flight.h264 &
 
 echo "Running  parseGPS.py"
-./Documents/HABcode/GPS/parseGPS.py
+./Documents/HABcode/GPS/parseGPS.py &
 
 echo "Running CO2 sensor"
-./Documents/HABcode/CO2/CO2.py
+./Documents/HABcode/CO2/CO2.py &
 
 echo "Running Geiger Counter"
-./Documents/HABcode/Geiger\ Counter/GeigerCounter.py
+./Documents/HABcode/Geiger\ Counter/GeigerCounter.py &
